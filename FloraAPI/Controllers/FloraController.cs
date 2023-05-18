f7ff1422a97ff03cd358cff6a7d6533ecd11994a
PawoOoOo <49211836+Pawopiiieee@users.noreply.github.com>
2023-05-18 22:54:33 +0200

    Add project files.

---
+++ FloraAPI/Controllers/FloraController.cs
@@ -0,0 +1,20 @@
+﻿using FloraAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FloraAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FloraController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllFlora()
+        {
+            var floraList = new List<Flora>
+            {
+                new Flora { Id = 1,}
+            }
+        }
+    }
+}
---
+++ FloraAPI/Models/Flora.cs
@@ -0,0 +1,13 @@
+﻿namespace FloraAPI.Models
+{
+    public class Flora
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Kingdom { get; set; } = string.Empty;
+        public string Family { get; set; } = string.Empty;
+        public string Genus { get; set; } = string.Empty;
+        public string Species { get; set; } = string.Empty;
+
+    }
+}
