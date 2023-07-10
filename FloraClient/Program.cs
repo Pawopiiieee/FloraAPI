b15b0d18b4e404048e5aae3c5c5c00c9eaa251f0
PawoOoOo <49211836+Pawopiiieee@users.noreply.github.com>
2023-07-10 17:31:47 +0200

    Connect HttpClient to APIs

--- FloraClient/Program.cs
+++ FloraClient/Program.cs
@@ -1,14 +1,18 @@
-using FloraClient.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-
+builder.Services.AddHttpClient(); //add http client factory into the system to access end point
+builder.Services.AddHttpClient("Flora", c =>
+{
+    var baseAddress = builder.Configuration.GetValue<string>("FloraAPI");
+    if (baseAddress is null)
+    {
+        throw new ArgumentNullException("FloraAPI");
+    }
+    c.BaseAddress = new Uri(baseAddress); //uri can't  be null
+}); 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
